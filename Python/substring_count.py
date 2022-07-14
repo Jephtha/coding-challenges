814c9350a96f6b6e4a8b2aa348fc94e112c980e5
Jeff Anga <44814755+Jephtha@users.noreply.github.com>
2022-07-14 13:30:00 -0230

    Create substring_count.py

---
+++ Python/substring_count.py
@@ -0,0 +1,4 @@
+# Write a program to find how many times substring x appears in the given string
+# Example: find how many times substring “Emma” appears in the given string
+# str_x = "Emma is good developer. Emma is a writer"
+
