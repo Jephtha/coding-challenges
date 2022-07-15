2bd521fbd1ff612008f3a5b1eee997b5805f9c05
Jeff Anga <44814755+Jephtha@users.noreply.github.com>
2022-07-15 17:01:14 -0230

    Delete substring_count.py

--- Python/substring_count.py
+++
@@ -1,8 +0,0 @@
-# Write a program to find how many times substring x appears in the given string
-# Example: find how many times substring “Emma” appears in the given string
-# str_x = "Emma is good developer. Emma is a writer"
-
-x = "Emma"
-str_x = "Emma is good developer. Emma is a writer"
-
-print(str_x.count(x))
