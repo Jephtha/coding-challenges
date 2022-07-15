6eb748be38a37b3af39ed6a60ce471a6719adfe6
Jeff Anga <44814755+Jephtha@users.noreply.github.com>
2022-07-14 22:02:21 -0230

    Update substring_count.py

--- Python/substring_count.py
+++ Python/substring_count.py
@@ -2,3 +2,7 @@
 # Example: find how many times substring “Emma” appears in the given string
 # str_x = "Emma is good developer. Emma is a writer"
 
+x = "Emma"
+str_x = "Emma is good developer. Emma is a writer"
+
+print(str_x.count(x))
