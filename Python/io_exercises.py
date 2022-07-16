a89547eee2be3bb67d4abd476bc8fcb6e76d732a
Jeff Anga <44814755+Jephtha@users.noreply.github.com>
2022-07-16 20:10:14 -0230

    Add files via upload

---
+++ Python/io_exercises.py
@@ -0,0 +1,13 @@
+# Python Input and Output Exercises from this website: https://pynative.com/python-input-and-output-exercise/
+
+
+# Exercise 1
+# Write a program to accept two numbers from the user and calculate multiplication
+# x, y = input("Enter two numbers: ").split()
+# print(int(x)*int(y))
+
+# Exercise 2
+# print('Name', 'Is', 'James') as Name**Is**James
+# print('Name', 'Is', 'James', sep='**')
+
+# Exercise 3
