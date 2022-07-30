cb71f24cd8a0083e5d94bb315c4c1c5e5b4931b4
Jeff Anga <44814755+Jephtha@users.noreply.github.com>
2022-07-30 18:19:00 -0230

    Add files via upload

---
+++ Python/loops.py
@@ -0,0 +1,65 @@
+# # Python Loops Exercises from this website: https://pynative.com/python-if-else-and-for-loop-exercise-with-solutions/
+
+# Exercise 1: Print First 10 natural numbers using while loop
+# for i in range(1, 11):
+#     print(i)
+ 
+# Exercise 2: Print the following pattern
+# for i in range(1, 6):
+#     for j in range(1, i+1):
+#         print(j, end='')
+#     print()
+
+# Exercise 3: Calculate the sum of all numbers from 1 to a given number
+# x = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1, x+1):
+#     sum += i
+# print("sum: ", sum)
+
+# Exercise 4: Write a program to print multiplication table of a given number
+# num = 3
+# for i in range(1, 11):
+#     print(num * i)
+
+# Exercise 5: Display numbers from a list using loop
+# numbers = [12, 75, 150, 180, 145, 525, 50]
+# for x in numbers:
+#     if x > 500:
+#         break
+#     elif x > 150:
+#         continue
+#     elif x % 5 == 0:
+#         print(x)
+
+# Exercise 6: Count the total number of digits in a number
+# num = 75869
+# count = 0
+# for i in str(num):
+#     count += 1
+# print(count)
+
+# Exercise 7: Print the following pattern
+# for i in range(5, 0, -1):
+#     for j in range(i, 0, -1):
+#         print(j, end='')
+#     print()
+
+# Exercise 8: Print list in reverse order using a loop
+# list1 = [10, 20, 30, 40, 50]
+# for i in range(len(list1)):
+#     print(list1.pop())
+
+# Exercise 9: Display numbers from -10 to -1 using for loop
+# for i in range(-10, 0):
+#     print(i)
+
+# Exercise 10: Use else block to display a message “Done” after successful execution of for loop
+# Exercise 11: Write a program to display all prime numbers within a range
+# Exercise 12: Display Fibonacci series up to 10 terms
+# Exercise 13: Find the factorial of a given number
+# Exercise 14: Reverse a given integer number
+# Exercise 15: Use a loop to display elements from a given list present at odd index positions
+# Exercise 16: Calculate the cube of all numbers from 1 to a given number
+# Exercise 17: Find the sum of the series upto n terms
+# Exercise 18: Print the following pattern
