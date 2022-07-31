df7697c3803c3fd513ecacd28d53a5f40c71001a
Jeff Anga <44814755+Jephtha@users.noreply.github.com>
2022-07-31 18:37:41 -0230

    Add files via upload

--- Python/loops.py
+++ Python/loops.py
@@ -55,11 +55,56 @@
 #     print(i)
 
 # Exercise 10: Use else block to display a message “Done” after successful execution of for loop
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Done!")
+
 # Exercise 11: Write a program to display all prime numbers within a range
+# start = 25
+# end = 50
+# for i in range(start, end+1):
+#     for j in range(2, i):
+#         if (i % j) == 0:
+#             break
+#     else:
+#         print(i)
+
 # Exercise 12: Display Fibonacci series up to 10 terms
+# fib = [0, 1]
+# for i in range(1, 9):
+#     fib.append(fib[i] + fib[i-1])
+# print(fib)
+
 # Exercise 13: Find the factorial of a given number
+# factorial = 1
+# for i in range(1, 6):
+#     factorial *= i
+# print(factorial)
+                                                                                                                                                                           
 # Exercise 14: Reverse a given integer number
+# num = 76542
+# print(''.join(list(reversed(str(num)))))
+
 # Exercise 15: Use a loop to display elements from a given list present at odd index positions
+# my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# for i in my_list:
+#     if my_list.index(i) % 2 != 0:
+#         print(i)
+
 # Exercise 16: Calculate the cube of all numbers from 1 to a given number
+# for i in range(1, 7):
+#     print(i*i*i)
+
 # Exercise 17: Find the sum of the series upto n terms
-# Exercise 18: Print the following pattern
+# sum = 0
+# for i in range(1, 6):
+#     sum += int("2"*i)
+# print(sum)
+
+# # Exercise 18: Print the following pattern
+# for i in range(1, 5):
+#     print("*" * i)
+#     if i == 4:
+#         for j in range(5, 0, -1):
+#             print("*"*j)
